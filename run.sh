baaf5eec171933bfce078ec2e9ea32fb7d896df3
ZJC2000 <jichaozhu@mail.ustc.edu.cn>
2024-05-29 00:46:27 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+# python hf_download.py --model THUDM/chatglm-6b --save_dir ./hf_hub
+
+
+# python hf_download.py --model THUDM/glm-10b --save_dir ./hf_hub
+
+export name=
+python hf_download.py --model spacy/en_core_web_trf --save_dir ./gcn
+
+
+
+# python hf_download.py --dataset Aniemore/REPV --save_dir ./hf_hub
